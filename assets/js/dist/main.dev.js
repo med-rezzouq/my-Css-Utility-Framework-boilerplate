bc6eb6fc3457abf2627a30be912ab9d999b309d2
rezzouq <med.rezzouq9@gmail.com>
2021-03-02 00:55:35 +0100

    style(responsive + content): fixing responsive and adding mobile menu

---
+++ assets/js/dist/main.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
